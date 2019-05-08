3ee70bcfb51d67812778c58adeead545879550d2
Murphy McMahon <murphy@centriqhome.com>
2019-05-08 10:31:54 -0700

    add platform check to avoid running ios-specific code on android

--- RNUserDefaultsIOS.js
+++ RNUserDefaultsIOS.js
@@ -1,47 +1,51 @@
 'use strict';
 
-var { NativeModules } = require('react-native');
+var { NativeModules, Platform } = require('react-native');
 var Promise = require('bluebird'); // jshint ignore:line
 var UserDefaults = NativeModules.RNUserDefaultsIOS;
 
-var _setObjectForKey = Promise.promisify(UserDefaults.setObjectForKey);
-var _setBoolForKey = Promise.promisify(UserDefaults.setBoolForKey);
+if (Platform.OS === 'ios') {
+  var _setObjectForKey = Promise.promisify(UserDefaults.setObjectForKey);
+  var _setBoolForKey = Promise.promisify(UserDefaults.setBoolForKey);
 
-var _arrayForKey = Promise.promisify(UserDefaults.arrayForKey);
-var _stringForKey = Promise.promisify(UserDefaults.stringForKey);
-var _objectForKey = Promise.promisify(UserDefaults.objectForKey);
-var _boolForKey = Promise.promisify(UserDefaults.boolForKey);
+  var _arrayForKey = Promise.promisify(UserDefaults.arrayForKey);
+  var _stringForKey = Promise.promisify(UserDefaults.stringForKey);
+  var _objectForKey = Promise.promisify(UserDefaults.objectForKey);
+  var _boolForKey = Promise.promisify(UserDefaults.boolForKey);
 
-var _removeItemForKey = Promise.promisify(UserDefaults.removeObjectForKey);
+  var _removeItemForKey = Promise.promisify(UserDefaults.removeObjectForKey);
 
-var UserDefaults = {
+  var UserDefaults = {
     setArrayForKey(array, key) {
-        return _setObjectForKey(array, key);
+      return _setObjectForKey(array, key);
     },
     setStringForKey(string, key) {
-        return _setObjectForKey(string, key);
+      return _setObjectForKey(string, key);
     },
     setObjectForKey(object, key) {
-        return _setObjectForKey(object, key);
+      return _setObjectForKey(object, key);
     },
     setBoolForKey(bool, key) {
-        return _setBoolForKey(bool, key);
+      return _setBoolForKey(bool, key);
     },
     arrayForKey(key) {
-        return _arrayForKey(key);
+      return _arrayForKey(key);
     },
     stringForKey(key) {
-        return _stringForKey(key);
+      return _stringForKey(key);
     },
     objectForKey(key) {
-        return _objectForKey(key);
+      return _objectForKey(key);
     },
     boolForKey(key) {
-        return _boolForKey(key);
+      return _boolForKey(key);
     },
     removeItemForKey(key) {
-        return _removeItemForKey(key);
+      return _removeItemForKey(key);
     }
-};
+  };
+} else {
+  var UserDefaults = {};
+}
 
 module.exports = UserDefaults;
